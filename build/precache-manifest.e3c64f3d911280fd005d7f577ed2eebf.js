ce3cb8c574f6a648b3dcda2eeace24955a7cb21c
Арсений <Onsha.z@yandex.ru>
2020-07-28 19:08:16 +0300

    adding build folder to the repo

---
+++ build/precache-manifest.e3c64f3d911280fd005d7f577ed2eebf.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "682e29732b533c2f96ec3957b4634ec1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e87028909bbcd8651eac",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "d8faf5dbe4e6d10e2dc8",
+    "url": "/static/css/main.ac7c0a28.chunk.css"
+  },
+  {
+    "revision": "e87028909bbcd8651eac",
+    "url": "/static/js/2.b0eb153c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b0eb153c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8faf5dbe4e6d10e2dc8",
+    "url": "/static/js/main.ac6ed26a.chunk.js"
+  },
+  {
+    "revision": "0d5b09e5c87119eee976",
+    "url": "/static/js/runtime-main.a2d48374.js"
+  },
+  {
+    "revision": "409c7f79a42e56c785f50ed37535f0be",
+    "url": "/static/media/Montserrat-Light.409c7f79.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "3ea8d4de05540a021026a2e8a2cf24eb",
+    "url": "/static/media/bkg.3ea8d4de.jpg"
+  },
+  {
+    "revision": "853dc6fba3d23ffbc1a7f1fc947fe683",
+    "url": "/static/media/down-arrow.853dc6fb.svg"
+  }
+]);
